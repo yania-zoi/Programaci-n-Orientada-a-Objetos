f326e0ddebfeecd7193b29ed81f551fe6e973423
yania-zoi <yaniazambu@gmail.com>
2026-08-25 08:34:17 -0600

    Create sumadedosnumeros.cpp

---
+++ sumadedosnumeros.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int a, b, c;
+    cout<<"SUMA DE DOS NUMEROS \n \n";
+    cout<<"ingresa dos numeros enteros: \n";
+    cin>>a>>b;
+    c = a + b;
+    cout <<"la suma de "<<a<<" + "<<b<<" = "<<c<<endl;
+
+    return 0;
+}
