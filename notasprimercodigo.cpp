83d1d4b2ccefd4180885b555e05051ace94ac575
yania-zoi <yaniazambu@gmail.com>
2026-08-25 08:27:29 -0600

    Create notasprimercodigo.cpp

---
+++ notasprimercodigo.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    string nombre;
+    cout << "Buenos dias!, cual es tu nombre?\n";
+    cin >> nombre;
+    cout << "Hola, " << nombre << endl;
+
+    return 0;
+}
