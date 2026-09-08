487124466754824957556afcb719d150fc6bdb3d
yania-zoi <yaniazambu@gmail.com>
2026-09-08 08:15:46 -0600

    Create cafe.cpp

---
+++ cafe.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#define CLS "\033[2J\033[H"
+
+using namespace std;
+
+class producto{
+    private:
+        string nombre;
+        int precio;
+        string categoria;
+        string tamanio;
+        bool disponibilidad; //1 disponible 0 no hayyyy
+
+    public: //constructor
+
+};
+int main()
+{
+    cout<<"======CAFE MENU======"<<endl;
+    return 0;
+}
